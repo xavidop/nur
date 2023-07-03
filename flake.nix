081a719694c9e129748af08752a1a645ec48a679
xavidop <xavi_tb@hotmail.com>
2023-07-03 17:33:49 +0200

    fix: update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   outputs = { self, nixpkgs }:
     let
       systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
         "x86_64-linux"
         "i686-linux"
-        "x86_64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
@@ -14,9 +15,8 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
+      packages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
