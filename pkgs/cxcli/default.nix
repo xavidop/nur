d4d4f91370191d6a4f25b06983dea87d8e18d8e7
xavidop <xavi_tb@hotmail.com>
2023-07-03 17:19:52 +0200

    feat: added cxcli

--- pkgs/cxcli/default.nix
+++ pkgs/cxcli/default.nix
@@ -1,7 +1,7 @@
 { stdenv }:
 
 stdenv.mkDerivation rec {
-  name = "example-package-${version}";
+  name = "cxcli-${version}";
   version = "1.0";
   src = ./.;
   buildPhase = "echo echo Hello World > example";
